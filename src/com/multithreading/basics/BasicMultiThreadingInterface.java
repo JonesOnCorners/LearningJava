07aff1c204c0fa392cd686d3c516b833e81c9d5c
JonesOnCorners <jayd.karale@gmail.com>
2019-02-19 23:49:38 +0530

    hello

--- src/com/multithreading/basics/BasicMultiThreadingInterface.java
+++ src/com/multithreading/basics/BasicMultiThreadingInterface.java
@@ -1,9 +1,23 @@
 package com.multithreading.basics;
 
+
+class BasicMultiThreadingRun implements Runnable{
+	public void run() {
+		System.out.println("Displaying Thread with ID = "+ Thread.currentThread().getId());
+	}
+}
+
 public class BasicMultiThreadingInterface {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		
+		for (int i=0;i>10;i++) {
+			Thread object = new Thread(new BasicMultiThreadingRun());
+			object.start();
+		}
 
 	}
 
